e28e9fd0a00c687190d2ef3e61cbb2a152123b92
Justin Sun <i@jsun969.cn>
2022-06-19 22:41:57 +0800

    chore: init css module

--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,25 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
     ],
   },
   resolve: {
