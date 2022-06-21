93478cf32fc2608ff018ae51001f7d9aba2beafd
Justin Sun <i@jsun969.cn>
2022-06-21 22:28:20 +0800

    feat: add gray icon and mock enable data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-chrome.devtools.panels.create(
-  '📋 React Hook Form',
-  'images/icon64.png',
-  'devtools.html',
-);
+import { enable } from './mock-data';
+
+if (enable) {
+  chrome.devtools.panels.create(
+    '📋 React Hook Form',
+    'images/icon64.png',
+    'devtools.html',
+  );
+}
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,16 @@
+import { enable } from './mock-data';
 import Popup from './popup/Popup';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+if (enable) {
+  chrome.action.setIcon({
+    path: { 16: 'images/icon16.png', 32: 'images/icon32.png' },
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Popup enable={true} />
+    <Popup enable={enable} />
   </React.StrictMode>,
 );
